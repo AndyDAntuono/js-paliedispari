05f2903e3c959c7d7f978d6739177f21c322eb51
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-04 21:19:51 +0200

    [UPD] updated script.js

--- Pari_e_dispari/js/script.js
+++ Pari_e_dispari/js/script.js
@@ -1,19 +1,25 @@
+//permetto all'utente si scegliere tra "pari" e "dispari"
+const evenOrOdd = prompt("Scegli o pari o dispari")
+
 //permetto l'inserimento di un numero che vada da 1 a 5 da parte dell'utente
-const userNumber = parseInt(prompt("Digita un numero che vada 1 a 5: "));
-console.log(a)
+const insertNumber = prompt("Digita un numero che vada 1 a 5: ");
+let userNumber = parseInt(insertNumber);
+console.log(userNumber)
 
 //imposto una funzione che crei un numero randomico che parta 1 fino a 5
 let computerNumber = Math.floor(Math.random() * 5) + 1; 
-console.log(b)
+console.log(computerNumber)
+
+let somma = userNumber + computerNumber;
+console.log(somma)
 
 //imposto una funzione che mi pemette di addzionare il numero inserito dall'utente con quello randomico del computer
 
+/*
 function controllo_somma() {
     
     let somma = userNumber+computerNumber;
-
-    if ((somma % 2 == 0) = (userNumber % 2 == 0) {
-        console.log("L'utente vince")
-    }
+    console.log(somma)
 
 }
+*/
