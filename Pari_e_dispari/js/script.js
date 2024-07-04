21807a03827d03162055cb44b0791e70705afb43
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-04 18:03:26 +0200

    [UPD] fixed index.html; updated script.js

---
+++ Pari_e_dispari/js/script.js
@@ -0,0 +1,12 @@
+//creo una costante che permetta l'inserimento del numero dell'utente
+const a = prompt("Digita un numero da 1 a 5: ")
+console.log(a)
+
+//creo una funzione che permette di generare un numero randomico per il computer
+
+function computer_random_number_generator() {
+    let b = (Math.floor() * 5 + 1)
+    console.log(b)
+}
+
+//creo una funziona che mi permetta di capire se la somma dei due numeri è pari o dispari
