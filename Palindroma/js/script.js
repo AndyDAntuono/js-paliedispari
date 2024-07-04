04c99b8bae0fe1dd206052d796b22ef190e8438e
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-04 17:23:06 +0200

    [UPD] updated scipt.js

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,14 +1,17 @@
-//Permetto l'inserimento della parola da parte dell'utente
-let parola = prompt("Digita una parola")
-
 //creo una funzione che ha il compito di suddividere, invertie e riunire le lettere della parola inserita dall'utente
-function contollo_parola(parola) {
-    let palindromo = parola.split("").reverse().join();
+function contollo_palindromo(parola) {
 
-    if (parola === palindromo.replaceAll (",", "")) {
-        return true;
+    const palindromo = parola.split("").reverse().join("");
+
+    if (parola === contollo_palindromo) {
+        console.log("la stringa è palindroma")
     }
     else {
-        return false;
+        console.log("La stringa non è palindroma")
     }
 }
+
+//Permetto l'inserimento della parola da parte dell'utente
+const parola = prompt("Digita una parola: ")
+
+contollo_palindromo(parola)
