8692499d8963744fc76514a4eb56a29e214df51c
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-04 20:32:28 +0200

    [UPD] updated both script.js files

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,9 +1,9 @@
 //creo una funzione che ha il compito di suddividere, invertie e riunire le lettere della parola inserita dall'utente
 function contollo_palindromo(parola) {
 
-    const palindromo = parola.split("").reverse().join("");
+    let reverseWord = parola.split("").reverse().join("");
 
-    if (parola === palindromo) {
+    if (parola === reverseWord) {
         console.log("la stringa è palindroma")
     }
     else {
--- Pari_e_dispari/js/script.js
+++ Pari_e_dispari/js/script.js
@@ -1,7 +1,19 @@
 //permetto l'inserimento di un numero che vada da 1 a 5 da parte dell'utente
-const a = parseInt(prompt("Digita un numero che vada 1 a 5: "));
+const userNumber = parseInt(prompt("Digita un numero che vada 1 a 5: "));
 console.log(a)
 
 //imposto una funzione che crei un numero randomico che parta 1 fino a 5
-let b = Math.floor(Math.random() * 5) + 1; 
+let computerNumber = Math.floor(Math.random() * 5) + 1; 
 console.log(b)
+
+//imposto una funzione che mi pemette di addzionare il numero inserito dall'utente con quello randomico del computer
+
+function controllo_somma() {
+    
+    let somma = userNumber+computerNumber;
+
+    if ((somma % 2 == 0) = (userNumber % 2 == 0) {
+        console.log("L'utente vince")
+    }
+
+}
