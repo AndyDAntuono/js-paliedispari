e7236c5c7e91f48086c627c2c0bfa7805c679c99
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-04 19:08:33 +0200

    [UPD] updated script.js

--- Pari_e_dispari/js/script.js
+++ Pari_e_dispari/js/script.js
@@ -1,3 +1,7 @@
 //permetto l'inserimento di un numero che vada da 1 a 5 da parte dell'utente
-const a = parseInt(prompt("Digita un numero che vada 1 a 5: "))
+const a = parseInt(prompt("Digita un numero che vada 1 a 5: "));
 console.log(a)
+
+//imposto una funzione che crei un numero randomico che parta 1 fino a 5
+let b = Math.floor(Math.random() * 5) + 1; 
+console.log(b)
